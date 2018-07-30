c594e021289596d9bdc1c474cdd51c854612ef40
Charles Koll <kollch@oregonstate.edu>
2018-07-29 19:47:13 -0700

    Add touch and capture capabilities

--- index.js
+++ index.js
@@ -55,8 +55,8 @@ function startingLayout(opponentColor) {
       newPiece.src = "images/black" + pieces[i] + ".png";
     }
     newPiece.className = "black";
-    newPiece.setAttribute('draggable', true);
-    newPiece.setAttribute('ondragstart', "drag(event)");
+    //newPiece.setAttribute('draggable', true);
+    //newPiece.setAttribute('ondragstart', "drag(event)");
     /*
     newPiece.on("dragstart", function(event) {
       drag(event);
@@ -69,8 +69,8 @@ function startingLayout(opponentColor) {
     newPiece.id = "w" + pieceIds[i] + pieces[i];
     newPiece.src = "images/white" + pieces[i] + ".png";
     newPiece.className = "white";
-    newPiece.setAttribute('draggable', true);
-    newPiece.setAttribute('ondragstart', "drag(event)");
+    //newPiece.setAttribute('draggable', true);
+    //newPiece.setAttribute('ondragstart', "drag(event)");
     /*
     newPiece.on("dragstart", drag(event));
     */
@@ -86,8 +86,8 @@ function startingLayout(opponentColor) {
     if ((i / 8) % 2 < 1 && i % 2 == 1 || (i / 8) % 2 >= 1 && i % 2 == 0) {
       newElement.className = "boardSquare even";
     }
-    newElement.setAttribute('ondrop', "drop(event)");
-    newElement.setAttribute('ondragover', "allowDrop(event)");
+    //newElement.setAttribute('ondrop', "drop(event)");
+    //newElement.setAttribute('ondragover', "allowDrop(event)");
     /*
     newElement.on("drop", drop(event));
     newElement.on("dragover", allowDrop(event));
@@ -194,6 +194,97 @@ function startingLayout(opponentColor) {
   }
 }
 
-$(function() {
+$(() => {
   startingLayout("black");
+  document.onmousedown = e => {
+    let mouseX, mouseY, elmnt;
+    e.preventDefault();
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+    elmnt = document.elementFromPoint(mouseX, mouseY);
+    if (elmnt.tagName !== "IMG") {
+      return;
+    }
+
+    document.onmousemove = e => {
+      e.preventDefault();
+      let ePosX = mouseX - e.clientX;
+      let ePosY = mouseY - e.clientY;
+      mouseX = e.clientX;
+      mouseY = e.clientY;
+      elmnt.style.position = "absolute";
+      elmnt.style.top = (elmnt.offsetTop - ePosY) + "px";
+      elmnt.style.left = (elmnt.offsetLeft - ePosX) + "px";
+    };
+
+    document.onmouseup = () => {
+      elmnt.style.pointerEvents = "none";
+      let cell = document.elementFromPoint(mouseX, mouseY);
+      if (cell.classList.contains("acceptable")) {
+        cell.appendChild(elmnt);
+      } else if (cell.tagName === "IMG" && (cell.parentNode.classList.contains("acceptable") || true)) {
+        cell.parentNode.appendChild(elmnt);
+        if (cell.classList.contains("black")) {
+          document.getElementById("capturedOpponent").appendChild(cell);
+        } else {
+          document.getElementById("capturedOwn").appendChild(cell);
+        }
+      }
+      elmnt.style.pointerEvents = "auto";
+      elmnt.style.position = "unset";
+      elmnt.style.top = "unset";
+      elmnt.style.left = "unset";
+      document.onmousemove = null;
+      document.onmouseup = null;
+    };
+  };
+
+  document.ontouchstart = e => {
+    if (e.touches.length > 1) {
+      return;
+    }
+    let mouseX, mouseY, elmnt;
+    mouseX = e.touches[0].clientX;
+    mouseY = e.touches[0].clientY;
+    elmnt = document.elementFromPoint(mouseX, mouseY);
+    if (elmnt.tagName !== "IMG") {
+      return;
+    }
+    e.preventDefault();
+
+    document.ontouchmove = e => {
+      e.preventDefault();
+      let ePosX = mouseX - e.touches[0].clientX;
+      let ePosY = mouseY - e.touches[0].clientY;
+      mouseX = e.touches[0].clientX;
+      mouseY = e.touches[0].clientY;
+      elmnt.style.position = "absolute";
+      elmnt.style.top = (elmnt.offsetTop - ePosY) + "px";
+      elmnt.style.left = (elmnt.offsetLeft - ePosX) + "px";
+    };
+
+    document.ontouchend = () => {
+      elmnt.style.pointerEvents = "none";
+      let cell = document.elementFromPoint(mouseX, mouseY);
+      if (cell.classList.contains("acceptable")) {
+        cell.appendChild(elmnt);
+      }
+      elmnt.style.pointerEvents = "auto";
+      elmnt.style.position = "unset";
+      elmnt.style.top = "unset";
+      elmnt.style.left = "unset";
+      document.ontouchmove = null;
+      document.ontouchend = null;
+      document.ontouchcancel = null;
+    };
+
+    document.ontouchcancel = () => {
+      elmnt.style.position = "unset";
+      elmnt.style.top = "unset";
+      elmnt.style.left = "unset";
+      document.ontouchmove = null;
+      document.ontouchend = null;
+      document.ontouchcancel = null;
+    };
+  };
 });
