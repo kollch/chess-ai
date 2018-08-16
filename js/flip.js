980ffb2e3a7e78d1862d7c2c572369420dc67073
Charles Koll <kollch@oregonstate.edu>
2018-08-16 08:35:06 -0700

    Add rotation after each move

--- js/flip.js
+++ js/flip.js
@@ -1,9 +1,32 @@
 function flip() {
-  let board = document.getElementById("board");
-  let captured = document.getElementById("captured");
-  let capturedOwn = document.getElementById("capturedOwn");
-  let capturedOpponent = document.getElementById("capturedOpponent");
-  board.append(...Array.from(board.childNodes).reverse());
+  const board = document.getElementById("board");
+  const boardCells = board.childNodes;
+  const captured = document.getElementById("captured");
+  const capturedOwn = document.getElementById("capturedOwn");
+  const capturedOpponent = document.getElementById("capturedOpponent");
+  const pieces = document.querySelectorAll("#board img");
+  const fromColor = capturedOwn.classList.contains("white") ? "white" : "black";
+  board.classList.toggle("showRot");
+  toggleClass(pieces, "showRot");
+  if (fromColor === "white") {
+    board.classList.toggle("rotClock");
+    toggleClass(pieces, "rotCClock");
+  } else {
+    board.classList.toggle("rotCClock");
+    toggleClass(pieces, "rotClock");
+  }
+  setTimeout(() => {
+    board.classList.toggle("showRot");
+    toggleClass(pieces, "showRot");
+    if (fromColor === "white") {
+      board.classList.toggle("rotClock");
+      toggleClass(pieces, "rotCClock");
+    } else {
+      board.classList.toggle("rotCClock");
+      toggleClass(pieces, "rotClock");
+    }
+    board.append(...Array.from(boardCells).reverse());
+  }, 2000);
   capturedOwn.id = "capturedOpponent";
   capturedOpponent.id = "capturedOwn";
   captured.append(...Array.from(captured.childNodes).reverse());
